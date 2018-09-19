9ea692c2d32fb497dc5008ffdb45234521a7ef0b
haythame <haytham.elzahar@thetrainline.com>
2018-09-19 12:10:25 +0100

    Tests

---
+++ haytham-seniorautomationengineer/Framework/PageObjectModels/BaseSearchPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace HaythamSeniorAutomationEngineer.Tests.Framework.PageObjectModels
+{
+    public abstract class BaseSearchPage
+    {
+        [FindsBy(How = How.Id, Using = "results")]
+        public IWebElement SearchResult { get; set; }
+
+        [FindsBy(How = How.Id, Using = "no-results-message")]
+        public IWebElement NotFound { get; set; }
+    }
+}
---
+++ haytham-seniorautomationengineer/Steps/RestaurantSearchStep.cs
@@ -0,0 +1,50 @@
+﻿using HaythamSeniorAutomationEngineer.Tests.Framework.PageObjectModels;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace HaythamSeniorAutomationEngineer.Tests.Steps
+{
+    [Binding]
+    public class RestaurantSearchSteps
+    {
+        private readonly IWebDriver _driver;
+        private SearchPage _searchPage;
+
+        public RestaurantSearchSteps(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _searchPage = PageFactory.InitElements<SearchPage>(_driver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _driver.Quit();
+        }
+
+        [Given(@"I want food in (.*)")]
+        public void GivenIWantFoodIn(string input)
+        {
+            _searchPage.Navigate();
+            _searchPage.Search(input);
+        }
+
+        [When(@"I search for restaurants")]
+        public void WhenISearchForRestaurants()
+        {
+
+        }
+
+        [Then(@"I should see some restaurants in (.*)")]
+        public void ThenIShouldSeeSomeRestaurantsIn(string p0)
+        {
+
+        }
+    }
+}
